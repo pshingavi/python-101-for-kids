e38411f79c5eaef47bf101c5f0a58313c08aaff7
Preetam Shingavi <preetams3@gmail.com>
2021-09-23 02:16:52 -0700

    Update guess_the_number_question.py

--- 1_guess_my_number/guess_the_number_question.py
+++ 1_guess_my_number/guess_the_number_question.py
@@ -3,3 +3,7 @@ import random
 # Select random number between 0 and 10, say magic_number
 
 # Keep asking user input as number until the input is equal to the magic_number
+# Provide user hint if the value is too high or low
+  #If user input < magic_number then print 'Too low', if user input > magic_number the print 'Too high'
+    
+# Finally print("You win !!!")
