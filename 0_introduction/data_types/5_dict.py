d5fb467c7a565c11f357d3807ed823d90bb870e7
Preetam Shingavi <preetams3@gmail.com>
2021-09-23 01:31:59 -0700

    Create 5_dict.py

---
+++ 0_introduction/data_types/5_dict.py
@@ -0,0 +1,24 @@
+# Empty dictionary
+my_dict = {"1": "Alice"}
+
+# Set key
+my_dict["2"] = "Judy"
+my_dict["3"] = "Amy"
+
+# Print
+my_dict
+
+# Overwrite existing key
+my_dict["3"] = "abc"
+
+# Print
+my_dict
+
+# Get value
+my_dict.get('2')
+
+# Get all keys
+my_dict.keys()
+
+# Get all values
+my_dict.values()
