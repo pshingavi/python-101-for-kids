7ed5c76f54296aeb050bd31c5b81909192e98080
Preetam Shingavi <preetams3@gmail.com>
2021-09-23 01:37:00 -0700

    Update 6_bool.py

--- 0_introduction/data_types/6_bool.py
+++ 0_introduction/data_types/6_bool.py
@@ -1,1 +1,11 @@
+# This is a boolean
+is_python = True
 
+# Everything in python can cast to be boolean
+is_python = bool("any thing here")
+
+# All of these are False
+these_are_false = bool(False or 0 or "" or {} or [] or None)
+
+# Most everything else is True
+these_are_true = bool(True and 1 and "non empty" and {"1": "abc"} and [1,2,3])
