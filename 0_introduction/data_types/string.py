37ade4f7fb4161875617156d44c95a2245234ad7
Preetam Shingavi <preetams3@gmail.com>
2021-09-23 01:16:01 -0700

    Create string.py

---
+++ 0_introduction/data_types/string.py
@@ -0,0 +1,13 @@
+# This is a string
+message = "Welcome to Python coding !!!"
+
+# This is also a string
+another_message = 'Welcome to Python coding !!!'
+
+# This is a multiline string
+multi_line_message = '''This is a multi-line message, first line 
+and another line'''
+
+# This is also a multiline string
+another_multi_line_message = """This is a multi-line message, first line 
+and another line"""
