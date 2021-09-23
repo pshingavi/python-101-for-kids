f62d94329f9f8b4ad27a94c7ce44a7af30bf7164
Preetam Shingavi <preetams3@gmail.com>
2021-09-23 01:26:34 -0700

    Create 4_lists.py

---
+++ 0_introduction/data_types/4_lists.py
@@ -0,0 +1,19 @@
+# Lists can be heterogeneous
+my_list = [1, "Hello"]
+
+# Appending
+my_list.append("world")
+
+# Extending
+my_list.extend(["Welcome", True])
+
+# Length of list
+len(my_list)
+
+# Print given a index
+my_list[0]
+
+# Print range of values
+my_list[0:2]
+
+my_list[2:]
