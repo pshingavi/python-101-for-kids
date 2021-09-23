12ba7d819fa73fb36311daa22fcfbf7a0dc08bba
Preetam Shingavi <preetams3@gmail.com>
2021-09-23 02:20:32 -0700

    Create 1_pre_requisite.py

---
+++ 1_guess_my_number/1_pre_requisite.py
@@ -0,0 +1,14 @@
+import random
+
+# Randome int
+random_num = random.randint(100, 200)
+print(random_num)
+
+
+# User input as string
+user_input = input("Provide user string input: ")
+print(user_input)
+
+# Case user input to int
+user_input = input("Provide user int input: ")
+print(int(user_input))
