23f00f54d22b5381bcd2d6401a70a841e841453b
Preetam Shingavi <preetams3@gmail.com>
2021-09-23 01:18:38 -0700

    Create number.py

---
+++ 0_introduction/data_types/number.py
@@ -0,0 +1,11 @@
+# Integer Numbers
+year = 2021
+year = int("2021")
+
+# Floating point Numbers
+pi = 3.14
+pi = float("3.14")
+
+# Fixed Point Numbers
+from decimal import Decimal
+price = Decimal("0.02")
