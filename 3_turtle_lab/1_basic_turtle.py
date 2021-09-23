d4b3356992ab4fb4eab9575779d10eff59a5d997
Preetam Shingavi <preetams3@gmail.com>
2021-09-23 15:13:47 -0700

    Update 1_basic_turtle.py

--- 3_turtle_lab/1_basic_turtle.py
+++ 3_turtle_lab/1_basic_turtle.py
@@ -1,1 +1,16 @@
+# Draw square spiral
 
+# Draw x pixels long line, turn 90 degree
+
+import turtle
+
+# Get turtle Pen
+t = turtle.Pen()
+
+# Draw fast
+t.speed(0)
+
+for x in range(100):
+    t.forward(x)
+    #t.forward(2*x)
+    t.left(90)
